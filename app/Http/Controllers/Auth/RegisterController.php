1d10bf98657039fb1e4a68054a0a8f2e1074ceb6
y.akamatsu <ak1985tk@gmail.com>
2018-08-28 09:55:26 +0000

    post list

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'company_name' => 'required|string|max:255',
+            'company_name' => 'required|string|max:255|unique:companies,name',
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
